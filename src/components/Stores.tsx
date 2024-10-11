48ff2e1b741c3b1d97270a8078a175cb55e32f1a
Karl Ludwig Weise <ludwig@ludwigweise.de>
2024-10-11 16:19:03 +0200

    Added useEffect to show state change

--- src/components/Stores.tsx
+++ src/components/Stores.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
 import { stores, type Brand } from "../stores";
 
 interface Props {}
 
 const Stores = ({}: Props) => {
+  useEffect(() => {
+    setTimeout(() => {
+      stores.brand.set({ name: "Example 2" });
+    }, 100);
+  });
   const brand: Brand = {
     name: "Example",
   };
